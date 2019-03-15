feb1d8b754dea3a6de8af65923fd0fcfc53a6c2e
wils0651 <wils0651@hotmail.com>
2019-03-15 03:43:13 -0600

    Can read a test file

---
+++ GCodeParser/Machine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCodeParser
+{
+    public class Machine
+    {
+        float X { get; set; }
+        float Y { get; set; }
+        float Z { get; set; }
+
+        float MaxVelocityX { get; set; }
+        float MaxVelocityY { get; set; }
+        float MaxVelocityZ { get; set; }
+
+        float AccelerationX { get; set; }
+        float AccelerationY { get; set; }
+        float AccelerationZ { get; set; }
+
+        float DurationSeconds { get; set; }
+
+        public Machine()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+
+
+    }
+}
--- GCodeParser/Program.cs
+++ GCodeParser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,31 @@ namespace GCodeParser
     {
         static void Main(string[] args)
         {
-            Console.Write("hello nerds");
-            Console.ReadLine();
+            string filePath = $"C:\\Users\\Tim\\Documents\\GitHub\\GCodeParser\\GCodeParser\\TestGCode\\hugger1_2.ngc";
+
+            // Create Parser
+
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    int count = 0;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine("Line " + count + ": " + line);
+                        count++;
+                        // Pass each line to parser
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
 
             // Open the file
 
@@ -20,6 +44,11 @@ namespace GCodeParser
             // parse
 
             // output results
+
+            Console.ReadLine();
         }
+
+
+
     }
 }
