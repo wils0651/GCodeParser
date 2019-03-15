8e6d3bdd17e41f971c9426883892cfe36a89bb51
wils0651 <wils0651@hotmail.com>
2019-03-15 03:06:15 -0600

    Added some files

---
+++ GCodeParser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCodeParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello nerds");
+            Console.ReadLine();
+
+            // Open the file
+
+            // get the words
+
+            // parse
+
+            // output results
+        }
+    }
+}
